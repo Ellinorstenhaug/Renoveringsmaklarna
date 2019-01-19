8f60f52ae9d627b38b7eab557ed308a4d62e765f
Alexander Helsinghof <alexander.helsinghof@cortinate.se>
2019-01-19 18:44:45 +0100

    fixat jpeg bugg

--- src/components/views/ServicePage.vue
+++ src/components/views/ServicePage.vue
@@ -1,77 +1,79 @@
 <template>
-  <v-content>
-    <section>
-    
-      <v-parallax :src="this.getImgUrl(this.testMethod())" v-if="$vuetify.breakpoint.mdAndUp" class="service-overlay">
-        <v-layout column align-center justify-center class="white--text">
-          <div class="jumbotron">
-            <div class="jumbo-container">
-              <h1 class="white--text mb-2 main-heading text-xs-center big-text">{{ this.service.heading
-                }}</h1>
-              <div class="subheading mb-3 text-xs-center">{{this.service.ingress}}</div>
-              <div class="button-wrapper">
-                    <Modal :service="this.service" :allServices="this.services" :quantity="4" :showDialog="this.activateDialog"/>
+  <div>
+
+    <v-content>
+      <section>
+
+        <v-parallax :src="this.category.overlay" v-if="$vuetify.breakpoint.mdAndUp" class="service-overlay">
+          <v-layout column align-center justify-center class="white--text">
+            <div class="jumbotron">
+              <div class="jumbo-container">
+                <h1 class="white--text mb-2 main-heading text-xs-center big-text">{{ this.service.heading}}</h1>
+                <div class="subheading mb-3 text-xs-center">{{this.service.ingress}}</div>
+                <div class="button-wrapper">
+                  <Modal :service="this.service" :allServices="this.services" :quantity="4" :showDialog="this.activateDialog" />
+                </div>
               </div>
             </div>
-          </div>
 
-        </v-layout>
-      </v-parallax>
-      <v-img :src="this.getImgUrl(this.category.smallImage)" alt="skriv något här" v-if=" $vuetify.breakpoint.smAndDown" class="service-overlay-mobil">
-        <v-layout column align-center justify-center class="white--text" style="background: #1b0f187d; ">
-          <div class="jumbo-container">
-            <h1 class="white--text mb-2 main-heading text-xs-center big-text">{{ this.service.heading }}</h1>
-            <div class="subheading mb-3 text-xs-center">{{this.service.ingress}}</div>
-            <div class="button-wrapper">
-              <Modal :service="this.service" :allServices="this.services" :showDialog="this.activateDialog" />
+          </v-layout>
+        </v-parallax>
+        <v-img :src="this.category.smallImage" alt="skriv något här" v-if=" $vuetify.breakpoint.smAndDown" class="service-overlay-mobil">
+          <v-layout column align-center justify-center class="white--text" style="background: #1b0f187d; ">
+            <div class="jumbo-container">
+              <h1 class="white--text mb-2 main-heading text-xs-center big-text">{{ this.service.heading }}</h1>
+              <div class="subheading mb-3 text-xs-center">{{this.service.ingress}}</div>
+              <div class="button-wrapper">
+                <Modal :service="this.service" :allServices="this.services" :showDialog="this.activateDialog" />
+              </div>
             </div>
-          </div>
+          </v-layout>
+        </v-img>
+      </section>
+      <section>
+        <v-layout column wrap align-center>
+          <v-flex xs12>
+            <v-card-title primary-title class="layout justify-center hidden-sm-and-down">
+              <h2 class="headline my-5 text-xs-center">{{this.service.subheading}} {{this.service.heading}}</h2>
+            </v-card-title>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center justify-center>
+                <v-flex xs12 md6>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Kom igång redan idag!</div>
+                    </v-card-title>
+                    <v-card-text>{{this.service.caption}}</v-card-text>
+                    <v-flex xs12 md8 offset-md2 align-center justify-center>
+                      <v-btn class="purple lighten-2 mt-2 set-width-100" dark large @click="activateDialog = !activateDialog">Intresserad?</v-btn>
+                    </v-flex>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md6>
+                  <v-card class="elevation-0 transparent hidden-sm-and-down">
+                    <v-img :src="this.category.subImage" class="img-wrapper grey lighten-2 service-img" aspect-ratio="1" />
+                  </v-card>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-flex>
         </v-layout>
-      </v-img>
-    </section>
-    <section>
-      <v-layout column wrap align-center>
-        <v-flex xs12>
-          <v-card-title primary-title class="layout justify-center hidden-sm-and-down">
-            <h2 class="headline my-5 text-xs-center">{{this.service.subheading}} {{this.service.heading}}</h2>
-          </v-card-title>
-          <v-container grid-list-xl>
-            <v-layout row wrap align-center justify-center>
-              <v-flex xs12 md6>
-                <v-card class="elevation-0 transparent">
-                  <v-card-title primary-title class="layout justify-center">
-                    <div class="headline text-xs-center">Kom igång redan idag!</div>
-                  </v-card-title>
-                  <v-card-text>{{this.service.caption}}</v-card-text>
-                  <v-flex xs12 md8 offset-md2 align-center justify-center>
-                    <v-btn class="purple lighten-2 mt-2 set-width-100" dark large @click="activateDialog = !activateDialog">Intresserad?</v-btn>
-                  </v-flex>
-                </v-card>
-              </v-flex>
 
-              <v-flex xs12 md6>
-                <v-card class="elevation-0 transparent hidden-sm-and-down">
-                  <!-- <v-img :src="this.getImgUrl(this.category.subImage)" class="img-wrapper grey lighten-2 service-img" aspect-ratio="1" /> -->
-                </v-card>
-              </v-flex>
-            </v-layout>
-          </v-container>
-        </v-flex>
-      </v-layout>
-    </section>
-
-    <section class="section-service pb-5" align-center>
-      <v-layout column wrap align-center>
-        <v-flex xs12 sm4 class="mt-3">
-          <div class="text-xs-center my-4">
-            <h2 class="display-1 my-3">Våra tjänster</h2>
-            <hr>
-          </div>
-        </v-flex>
-        <Services :allServices="this.services" :quantity="8" />
-      </v-layout>
-    </section>
-  </v-content>
+      </section>
+      <section class="section-service pb-5" align-center>
+        <v-layout column wrap align-center>
+          <v-flex xs12 sm4 class="mt-3">
+            <div class="text-xs-center my-4">
+              <h2 class="display-1 my-3">Våra tjänster</h2>
+              <hr>
+            </div>
+          </v-flex>
+          <Services :allServices="this.services" :quantity="8" />
+        </v-layout>
+      </section>
+    </v-content>
+  </div>
 </template>
 
 <script>
@@ -84,8 +86,10 @@
 
   export default {
     mounted() {
+      this.categories = categoriesJson;
+
       this.initService();
-   
+
     },
 
     watch: {
@@ -103,39 +107,56 @@
         service: {},
         services: serviceJson,
         categories: categoriesJson,
-        category: {},
-         activateDialog: false,
+        category: [],
+        overlay: '',
+        activateDialog: false,
       };
     },
     methods: {
-     testMethod(){
-       let img = this.category.overlay;
-       console.log(img);
-       return img;
-     },
-      getCategoryByServiceId(categoryId){
-         
-             return this.categories.find(function(category){
-                
-               return category.id == categoryId;
-                });
+      getCategoryByServiceId(catId) {
 
-            },
+        return this.categories.filter(function (category) {
 
-    
+          return catId == category.id;
+        });
+      },
+
+      mapCategorToObject(currentCategory) {
+
+        let mappedObj = {
+          id: currentCategory[0].id,
+          name: currentCategory[0].name,
+          overlay: this.getImgUrl(currentCategory[0].overlay),
+          smallImage: this.getImgUrl(currentCategory[0].smallImage),
+          subImage: this.getImgUrl(currentCategory[0].subImage),
+          icon: currentCategory[0].icon,
+          color: currentCategory[0].color
+        }
+        return mappedObj
+      },
+
 
       getImgUrl(img) {
-        return require("../../assets/" + img);
+
+        return require("@/assets/" + img);
       },
 
       initService: function () {
-        this.service = this.findService();  
-        this.category = this.getCategoryByServiceId(this.service.categoryId);
+
+        this.service = this.findService();
+
+        if (this.service) {
+          let arrayCategory = this.getCategoryByServiceId(this.service.categoryId);
+          this.category = this.mapCategorToObject(arrayCategory);
+          this.overlay = this.category.overlay;
+        }
+
       },
 
       findService: function () {
         let isFound = false;
         let serviceObject = {};
+
 
         for (var i = 0; i < serviceJson.length; i++) {
           if (serviceJson[i].url == this.$route.path) {
@@ -248,7 +269,8 @@
   .jumbotron-service {
     margin-top: 200px;
   }
-/* 
+
+  /* 
   .jumbo-container2 {
     width: 600px;
   } */
@@ -260,9 +282,8 @@
 
 
   @media (max-width: 600px) {
-      .my-5 {
-        margin-bottom:0 !important;
-      }
+    .my-5 {
+      margin-bottom: 0 !important;
+    }
   }
-
 </style>
